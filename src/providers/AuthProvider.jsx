78594eb0e1fcff75fdd77f8cf416d4762ae16ea6
SadmanJawad <jawadnabil977@gmail.com>
2023-04-29 02:47:30 +0600

    implemented login and registration

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -14,10 +14,15 @@ const AuthProvider = ({children}) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
 
     const authInfo = {
-    user   
-     
+    user,
+    createUser,
+    signIn
+
     }
 
     return (
