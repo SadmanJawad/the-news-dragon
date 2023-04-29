e71032c71843be24024275bf73c8fdbf1e8a5dbd
SadmanJawad <jawadnabil977@gmail.com>
2023-04-29 19:26:22 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('environment variable' , import.meta.env.VITE_apiKey)
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDUskYxdIxWrQbPS81o6abFHWh6asgsQo8",
-  authDomain: "the-news-dragon-53ffb.firebaseapp.com",
-  projectId: "the-news-dragon-53ffb",
-  storageBucket: "the-news-dragon-53ffb.appspot.com",
-  messagingSenderId: "956501327892",
-  appId: "1:956501327892:web:9b8ef3b3e6ea887c446701"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId:  import.meta.env.VITE_PROJECTID,
+  storageBucket:  import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:  import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:  import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
